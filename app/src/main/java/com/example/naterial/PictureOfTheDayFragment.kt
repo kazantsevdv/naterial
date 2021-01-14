80de86c3f428a7a15fec2fdb07b23b40df93e2a1
Denis Kazantsev <denspa@mail.ru>
2021-01-14 19:27:27 +0700

    задание 1

---
+++ app/src/main/java/com/example/naterial/PictureOfTheDayFragment.kt
@@ -0,0 +1,82 @@
+package com.example.naterial
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import coil.api.load
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class PictureOfTheDayFragment : Fragment() {
+
+
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getData()
+                .observe(viewLifecycleOwner, { renderData(it) })
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        input_layout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
+            })
+        }
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    toast("Link is empty")
+                } else {
+                    image_view.load(url) {
+                        crossfade(true)
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                    description.text = serverResponseData.explanation
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+            }
+            is PictureOfTheDayData.Error -> {
+                toast(data.error.message)
+            }
+        }
+    }
+
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
+    }
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+    }
+}
